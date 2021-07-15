db7cb23446e00b4518a59cb2a4cba66790539bce
Victor Varela <vitaotapioqueiro@gmail.com>
2021-07-15 18:52:30 -0300

    add bg cassino

--- script.js
+++ script.js
@@ -10,55 +10,87 @@
 // para aparecer cada imagem, por dentro do FOR timing de reprodução do código de 0.5s
 
 // 
-const caixa1 = document.querySelector('.jogo_caixa1')
-const caixa2 = document.querySelector('.jogo_caixa2')
-const caixa3 = document.querySelector('.jogo_caixa3')
+const caixa1 = document.querySelector('#caixa1')
+const caixa2 = document.querySelector('#caixa2')
+const caixa3 = document.querySelector('#caixa3')
+const alavanca = document.querySelector('#alavanca')
+
 let position = 0
 let sorteio1 = 0
 let sorteio2 = 0
 let sorteio3 = 0
-
-sorteio1 = Math.floor(Math.random() * (8 - 0 + 1) + 1)
-sorteio2 = Math.floor(Math.random() * (8 - 0 + 1) + 1)
-sorteio3 = Math.floor(Math.random() * (8 - 0 + 1) + 1)
-
 let i = 0
+let timer = 900
 
-setInterval(() => {
+const estruturaCacaNiquel = () => {
+
+    caixa1.style.width = '120px'
+    caixa1.style.height = '360px'
+    caixa1.style.backgroundImage = "url(icones.png)"
+    caixa1.style.backgroundSize = 'cover'
     
-    if (i !== 194){
-        position = 10 * i * 2.5
-        caixa1.style.backgroundPosition = `center ${position}px`
-        caixa2.style.backgroundPosition = `center ${position}px`
-        caixa3.style.backgroundPosition = `center ${position}px`
-        i++
-        console.log(position)
-        console.log(i)
-    }
-    else{
-        caixa1.style.backgroundPosition = `center ${sorteio1*120}px`
-        caixa2.style.backgroundPosition = `center ${sorteio2*120}px`
-        caixa3.style.backgroundPosition = `center ${sorteio3*120}px`
-    }
     
-}, 900/30);
+    caixa2.style.width = '120px'
+    caixa2.style.height = '360px'
+    caixa2.style.backgroundImage = "url(icones.png)"
+    caixa2.style.backgroundSize = 'cover'
+    
+    caixa3.style.backgroundImage = "url(icones.png)"
+    caixa3.style.width = '120px'
+    caixa3.style.height = '360px'
+    caixa3.style.backgroundSize = 'cover'
+
+}
 
 
-caixa1.style.width = '120px'
-caixa1.style.height = '360px'
-caixa1.style.backgroundImage = "url(icones.png)"
-caixa1.style.backgroundSize = 'cover'
+const startGame = () => {
+    // desativa o botão de rodar o game
+    alavanca.disabled = true
+
+    // cria uma variável para depois armazenar o setInterval()
+    let rodaGame = undefined
+    
+    // seta para o estado inicial todas as variáveis
+    position = 0
+    sorteio1 = 0
+    sorteio2 = 0
+    sorteio3 = 0
+    i = 0
+    
+    // seta estrutura da rolagem
+    estruturaCacaNiquel()
 
 
-caixa2.style.width = '120px'
-caixa2.style.height = '360px'
-caixa2.style.backgroundImage = "url(icones.png)"
-caixa2.style.backgroundSize = 'cover'
+    // sorteia em cada variável a posição de parada
+    sorteio1 = Math.floor(Math.random() * (8 - 0 + 1) + 1)
+    sorteio2 = Math.floor(Math.random() * (8 - 0 + 1) + 1)
+    sorteio3 = Math.floor(Math.random() * (8 - 0 + 1) + 1)
+    
+    
+    rodaGame = setInterval(() => {
+        
+        if (i !== 194) {
+            position = 10 * i * 2.5
+            caixa1.style.backgroundPosition = `center ${position}px`
+            caixa2.style.backgroundPosition = `center ${position}px`
+            caixa3.style.backgroundPosition = `center ${position}px`
+            i++
+        }
+        else {
+            caixa1.style.backgroundPosition = `center ${sorteio1 * 120}px`
+            caixa2.style.backgroundPosition = `center ${sorteio2 * 120}px`
+            caixa3.style.backgroundPosition = `center ${sorteio3 * 120}px`
+            clearInterval(rodaGame)
+            alavanca.disabled = false
+        }
+        
+    }, timer / 30);
+    
+    
+    
+}
 
-caixa3.style.backgroundImage = "url(icones.png)"
-caixa3.style.width = '120px'
-caixa3.style.height = '360px'
-caixa3.style.backgroundSize = 'cover'
+estruturaCacaNiquel()
+alavanca.addEventListener('click', startGame)
 
 
-console.log('fim do código')
